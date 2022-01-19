2e4f3ae4fa5669a3b24347ce7b3c3eec3ae1114d
JEKA <jeka632212@gmail.com>
2022-01-20 01:18:35 +0300

    feat: downgrade ts for lint and some fixes

--- apps/web-app/src/app/app.component.ts
+++ apps/web-app/src/app/app.component.ts
@@ -1,10 +1,10 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
+  changeDetection: ChangeDetectionStrategy.OnPush,
   selector: 'gtd-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'web-app';
 }
